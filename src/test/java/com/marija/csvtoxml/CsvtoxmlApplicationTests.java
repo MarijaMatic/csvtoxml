bbe89d48eec81722aa0ad84364f4daf4394fb4be
Marija Matic <mrj.ivancic@gmail.com>
2019-11-13 19:50:46 +0100

    first commit

---
+++ src/test/java/com/marija/csvtoxml/CsvtoxmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marija.csvtoxml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvtoxmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
